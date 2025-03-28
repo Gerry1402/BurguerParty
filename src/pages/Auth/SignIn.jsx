b52c320dc82b1d547dfe397fbb75baeb0666b9ca
Gerry1402 <gerard.vello@gmail.com>
2025-03-28 18:02:18 +0100

    Estilos bootstrap implementados

--- src/pages/Auth/SignIn.jsx
+++ src/pages/Auth/SignIn.jsx
@@ -1,9 +1,19 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Form from 'react-bootstrap/Form';
+
 const SignIn = () => {
     return (
-        <>
-            
-        </>
+        <Card>
+            <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                    {`We'll never share your email with anyone else.`}
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+        </Card>
     );
-}
+};
 
 export default SignIn;
